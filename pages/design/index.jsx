6c5e88ebb527b5d4824ce45426e7c08dd376d454
Shikhi Tseren <shikhinee@gmail.com>
2022-07-18 09:04:55 +0800

    page

---
+++ pages/design/index.jsx
@@ -0,0 +1,24 @@
+//Next, React (core node_modules) imports must be placed here
+
+//import STORE from '@/store'
+
+//import LAYOUT from '@/layouts'
+
+//import VIEWS from '@/views'
+
+//import useFETCHER from '@/tools'
+
+//import COMPOSITES from '@/composites'
+
+//import COMPONENT from '@/components'
+
+
+const DesignPage = (props) => {
+	 return (
+		 <main className="">
+			
+		</main>
+	)
+};
+
+export default DesignPage;
